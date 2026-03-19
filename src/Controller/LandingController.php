3e5139bf729e106de167c583de8ed25142a2a197
bavxhack <44317199+bavxhack@users.noreply.github.com>
2026-03-19 15:34:44 +0100

    Add personal time-based landing page greeting

--- src/Controller/LandingController.php
+++ src/Controller/LandingController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Security\KeycloakUser;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class LandingController extends AbstractController
 {
@@ -18,9 +20,31 @@ class LandingController extends AbstractController
     #[Route('/', name: 'app_landing', methods: ['GET'])]
     public function __invoke(): Response
     {
+        $user = $this->getUser();
+
         return $this->render('landing/index.html.twig', [
             'links' => $this->landingLinks,
-            'user' => $this->getUser(),
+            'user' => $user,
+            'personalGreeting' => $this->buildPersonalGreeting($user),
         ]);
+    }
+
+    private function buildPersonalGreeting(?UserInterface $user): string
+    {
+        $hour = (int) (new \DateTimeImmutable())->format('G');
+        $salutation = match (true) {
+            $hour >= 5 && $hour < 12 => 'Guten Morgen',
+            $hour >= 12 && $hour < 18 => 'Guten Tag',
+            $hour >= 18 && $hour < 22 => 'Guten Abend',
+            default => 'Hallo',
+        };
+
+        $name = match (true) {
+            $user instanceof KeycloakUser && '' !== trim($user->getDisplayName()) => $user->getDisplayName(),
+            $user instanceof UserInterface => $user->getUserIdentifier(),
+            default => 'schön, dass du da bist',
+        };
+
+        return sprintf('%s, %s!', $salutation, $name);
     }
 }
